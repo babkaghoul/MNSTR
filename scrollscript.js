64fa20d93db1654aca545175856f1414062a726d
babkaghoul <bykonya007@gmail.com>
2024-10-02 00:05:00 +1000

    Add files via upload

---
+++ scrollscript.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', function () {
+    // Логика обработки прокрутки
+    if (window.scrollY > 0) {
+        console.log('Scrolling down');
+        // можно выполнить что-то специфическое при прокрутке
+    }
+});
