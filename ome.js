7dd350bae2e48b07f8ae97cc059b4202a5960edd
babkaghoul <bykonya007@gmail.com>
2024-10-02 14:51:33 +1000

    Add files via upload

--- ome.js
+++ ome.js
@@ -1,8 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'; 
+     
+     
 
 const supabaseUrl = 'https://pmujergqrtpzbowthaar.supabase.co'; // Замените на ваш URL 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdWplcmdxcnRwemJvd3RoYWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3OTQ5OTgsImV4cCI6MjA0MzM3MDk5OH0.7rKSsnLCuep6gwCYhpuszWl9NvX2sHKMWbh7xdcge84'; // Замените на ваш ключ
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// ... ваш код с использованием supabase ...
+
 
