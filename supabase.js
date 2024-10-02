63341ed502548d563ddf08a32708eb05c008669d
babkaghoul <bykonya007@gmail.com>
2024-10-02 17:11:15 +1000

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,21 +1,22 @@
-        // supabase.js
-        const initSupabase = () => {
-            const supabaseUrl = 'https://pmujergqrtpzbowthaar.supabase.co';
-            const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdWplcmdxcnRwemJvd3RoYWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3OTQ5OTgsImV4cCI6MjA0MzM3MDk5OH0.7rKSsnLCuep6gwCYhpuszWl9NvX2sHKMWbh7xdcge84'; 
+// supabase.js
+const supabaseUrl = 'https://pmujergqrtpzbowthaar.supabase.co'; // Ваш URL Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdWplcmdxcnRwemJvd3RoYWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3OTQ5OTgsImV4cCI6MjA0MzM3MDk5OH0.7rKSsnLCuep6gwCYhpuszWl9NvX2sHKMWbh7xdcge84'; // Ваш ключ API Supabase
 
-            const supabase = new Supabase(supabaseUrl, supabaseKey);
+const initSupabase = () => {
+  // Создайте экземпляр Supabase внутри функции
+  const supabase = new Supabase(supabaseUrl, supabaseKey); 
 
-            // Ваш код, работающий с Supabase
-            supabase
-                .from('users') 
-                .select('*')
-                .then(data => {
-                    console.log(data.data); 
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        };
+  // Теперь мы можем использовать supabase!
+  supabase
+    .from('users') // Замените 'users' на название вашей таблицы
+    .select('*')
+    .then(data => {
+      console.log(data.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
 
-        // Вызовите функцию initSupabase() после загрузки библиотеки Supabase
-        window.addEventListener('load', initSupabase);
+// Вызываем initSupabase() после загрузки Supabase-JS
+window.addEventListener('load', initSupabase);
